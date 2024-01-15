76c015c7e118e3e11934214565357bf2ccbfea46
Usman <alie25708@gmail.com>
2024-01-15 21:57:56 +0500

    Consumers.py added. Basic methods connect, disconnect and receive

---
+++ Backend/Home/consumers.py
@@ -0,0 +1,11 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class MyConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, text_data):
+        pass
