1deefa9d188977d8c193aea3a956a5d985aaa284
Usman <alie25708@gmail.com>
2024-01-15 22:04:31 +0500

    changed asgi.py connected my routes

--- Backend/Backend/asgi.py
+++ Backend/Backend/asgi.py
@@ -10,7 +10,18 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import Home.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
 
 application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            Home.routing.websocket_urlpatterns
+        )
+    ),
+})
