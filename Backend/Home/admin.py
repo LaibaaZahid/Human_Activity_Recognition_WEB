273c84bce7b81a88f6c30ba3912e8570eb62f514
Wajdan Bhatti <h.wajdan29@gmail.com>
2024-01-20 17:23:20 +0500

    User model created

--- Backend/Home/admin.py
+++ Backend/Home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from Home.models import User
 
+class userAdmin(admin.ModelAdmin):
+    list_display=('device_type', 'device_name')
+
+admin.site.register(User, userAdmin )    
 # Register your models here.
--- Backend/Home/models.py
+++ Backend/Home/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
+class User(models.Model):
+    device_type = models.CharField(max_length=50)
+    device_name = models.CharField(max_length=50)
 # Create your models here.
