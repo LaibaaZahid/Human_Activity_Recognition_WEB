8a77f9a298d350c20f0654e11fa4035fb0ce15eb
kashafashraf230 <kashafashraf412@gmail.com>
2024-01-20 15:21:30 +0500

    User Table Added

--- Frontend/create-react-app/src/views/sample-page/index.js
+++ Frontend/create-react-app/src/views/sample-page/index.js
@@ -1,20 +1,79 @@
+import { useState } from "react";
+
 // material-ui
-import { Typography } from '@mui/material';
+/*import { Typography } from '@mui/material';*/
+import {Table} from 'ui-component/table/table.js';
+import {Modal} from 'ui-component/table/Modal.jsx';
 
 // project imports
-import MainCard from 'ui-component/cards/MainCard';
+/*import MainCard from 'ui-component/cards/MainCard';*/
 
 // ==============================|| SAMPLE PAGE ||============================== //
 
-const SamplePage = () => (
-  <MainCard title="Sample Card">
-    <Typography variant="body2">
-      Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-      minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
-      in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
-      descent molls anim id est labours.
-    </Typography>
-  </MainCard>
-);
+const SamplePage = () => {
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const [rows, setRows] = useState([
+    {
+      page: "User 1",
+      description: "This user is using Smartphone",
+      status: "activated",
+    },
+    {
+      page: "User 2",
+      description: "This user is using Smartwatch",
+      status: "deactivated",
+    },
+    {
+      page: "User 3",
+      description: "This user is using Glasses",
+      status: "error",
+    },
+  ]);
+  const [rowToEdit, setRowToEdit] = useState(null);
+
+  const handleDeleteRow = (targetIndex) => {
+    setRows(rows.filter((_, idx) => idx !== targetIndex));
+  };
+
+  const handleEditRow = (idx) => {
+    setRowToEdit(idx);
+
+    setModalOpen(true);
+  };
+
+  const handleSubmit = (newRow) => {
+    rowToEdit === null
+      ? setRows([...rows, newRow])
+      : setRows(
+          rows.map((currRow, idx) => {
+            if (idx !== rowToEdit) return currRow;
+
+            return newRow;
+          })
+        );
+  };
+
+  return (
+    <div className="App">
+      <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
+      <button onClick={() => setModalOpen(true)} className="btn addbtn">
+        Add
+      </button>
+      {modalOpen && (
+        <Modal
+          closeModal={() => {
+            setModalOpen(false);
+            setRowToEdit(null);
+          }}
+          onSubmit={handleSubmit}
+          defaultValue={rowToEdit !== null && rows[rowToEdit]}
+        />
+      )}
+    </div>
+  );
+}
+
+  
 
 export default SamplePage;
